a90540f05b74ddc305adb316a3f818853d7fa389
phi_spiritlabs <elpulga.dev2021@gmail.com>
2022-09-11 10:05:48 +0700

    create get all update product

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productController");
-
 const router = express.Router();
 
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+} = require("../controllers/productController");
+
 router.route("/products").get(getAllProducts);
+router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
-const app = require("./app");
 const dotenv = require("dotenv");
 
+// config
 dotenv.config({ path: "backend/config/config.env" });
+
+const app = require("./app");
+const connectDatabase = require("./config/database");
+
+// connecting to database
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
